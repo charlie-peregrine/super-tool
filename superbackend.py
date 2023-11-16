b243698ea7a994d259840b37ef4197a1a29ba365
Charlie 3 Jordan <charlie@peregrineengineering.com>
2023-11-16 15:45:47 -0500

    added file picker dialog for open project and directory to the file menu

--- superbackend.py
+++ superbackend.py
@@ -3,7 +3,20 @@
 
 import tkinter as tk
 from tkinter import ttk
+from tkinter.filedialog import askopenfilename, askdirectory
 
 # file picking:
 # https://stackoverflow.com/questions/3579568/choosing-a-file-in-python-with-simple-dialog
 
+def open_project():
+    # filetypes section to pick a specific extension!
+    filename = askopenfilename(filetypes=[("Python", "*.py")])
+    print(filename)
+    # do something else with the filename
+    pass
+
+def open_workspace():
+    dirname = askdirectory()
+    print(dirname)
+    # do something else with the dirname
+    pass
