65bf69fe9437a05494046e47d68be952671b02f8
Charlie 3 Jordan <charlie@peregrineengineering.com>
2023-11-16 14:47:58 -0500

    added more to the top menu, added example project text, added empty plot panel, adjusted spacing for test frame, changed plot_frame to param_frame

--- gui.py
+++ gui.py
@@ -19,9 +19,18 @@ root.columnconfigure(0, weight=1)
 menubar = tk.Menu(root)
 root.config(menu=menubar)
 file_menu = tk.Menu(menubar)
+about_menu = tk.Menu(menubar)
 
+file_menu.add_command(label="New")
+file_menu.add_command(label="Open Project File")
+file_menu.add_command(label="Open Workspace")
+file_menu.add_separator()
 file_menu.add_command(label='Exit', command=root.destroy)
 menubar.add_cascade(label="File", menu=file_menu)
+
+about_menu.add_command(label="About us")
+about_menu.add_command(label="Version")
+menubar.add_cascade(label="About", menu=about_menu)
 
 ### Frame creation
 # main frame setup, row and column configure for initial size and resizeability
@@ -32,34 +41,55 @@ main_frame.grid_rowconfigure(1, minsize=200, weight=1)
 main_frame.grid_columnconfigure(1,  minsize=250, weight=1)
 
 # set up the sub-frames
-proj_frame = ttk.Frame(main_frame, borderwidth=5, relief="groove",
-                       height=500, width=300)
-test_frame = ttk.Frame(main_frame, borderwidth=5, relief="groove",
+proj_frame = ttk.Frame(main_frame, borderwidth=5, relief='groove',
+                       height=300, width=300)
+test_frame = ttk.Frame(main_frame, borderwidth=5, relief='groove',
                        height=100, width=300)
-plot_frame = ttk.Frame(main_frame, borderwidth=5, relief="groove",
+param_frame = ttk.Frame(main_frame, borderwidth=5, relief='groove',
                        height=100, width=300)
-statusbar_frame = ttk.Frame(main_frame) # nothing else so it's just a popup sort of deal
+plot_frame = ttk.Frame(main_frame, borderwidth=5, relief='groove',
+                       height=300, width=500)
+statusbar_frame = ttk.Frame(main_frame, borderwidth=2, relief='groove') # nothing else so it's just a popup sort of deal
 
 # place the frames in their grids, main_frame inside root and the rest
 # inside main_frame
 main_frame.grid(row=0,column=0, sticky="nesw")
-proj_frame.grid(row=0,column=0,columnspan=1, rowspan=2, sticky="nesw")
-test_frame.grid(row=0,column=1,columnspan=1, rowspan=1, sticky="nesw")
-plot_frame.grid(row=1,column=1,columnspan=1, rowspan=1, sticky="nesw")
-statusbar_frame.grid(row=2, column=0, columnspan=2, sticky="nesw")
+proj_frame.grid(row=0,column=0, columnspan=1, rowspan=2, sticky="nesw")
+test_frame.grid(row=0,column=1, columnspan=1, rowspan=1, sticky="nesw")
+param_frame.grid(row=1,column=1, columnspan=1, rowspan=1, sticky="nesw")
+plot_frame.grid(row=0,column=2, columnspan=1, rowspan=2, sticky="nesw")
+statusbar_frame.grid(row=2, column=0, columnspan=3, sticky="nesw")
 
 # example text inside the frames
-proj_text = ttk.Label(proj_frame, text="project")
-proj_text.grid(row=0,column=0)
+# proj_text = ttk.Label(proj_frame, text="project")
+# proj_text.grid(row=0,column=0)
 # test_text = ttk.Label(test_frame, text="test")
 # test_text.grid(row=0,column=0)
-plot_text = ttk.Label(plot_frame, text="plot")
-plot_text.grid(row=0,column=0)
+param_text = ttk.Label(param_frame, text="plot params")
+param_text.grid(row=0,column=0)
 statusbar_text = ttk.Label(statusbar_frame, text="Status Bar")
 statusbar_text.grid(row=0, column=0)
 
+### Project Frame Details
+proj_header = ttk.Label(proj_frame, text="Project Name") # @TODO font size up
+proj_header.grid(row=0, column=0, columnspan=3)
+project_title = tk.StringVar()
+proj_header_entry = ttk.Entry(proj_frame, textvariable=project_title)
+proj_header_entry.grid(row=0,column=3, columnspan=2)
+
+num_tests = 4
+project_test_entries = [tk.StringVar() for i in range(num_tests)]
+for i in range(num_tests):
+    ttk.Label(proj_frame, text="Test %d\n  Test type" % (i+1)).grid(
+        row=1+i, column=0, columnspan=2
+    )
+    ttk.Entry(proj_frame, textvariable=project_test_entries[i]).grid(
+        row=1+i, column=2, columnspan=3
+    )
+
+
 ### Test Parameters Box details
-test_header = ttk.Label(test_frame, text="Test Parameters:") #@TODO font size up 
+test_header = ttk.Label(test_frame, text="Test Parameters:") # @TODO font size up 
 test_header.grid(row=0, column=0, columnspan=2, sticky="w")
 
 # example input field to show that the entries work
@@ -70,10 +100,10 @@ lines = [("start time", strings[0], "sec"), ("end time", strings[1], "sec"),
          ("step up size", strings[2], "pu"), ("step down size", strings[3], "pu")]
 for i in range(len(lines)):
     test_name = ttk.Label(test_frame, text=lines[i][0])
-    test_input = ttk.Entry(test_frame, textvariable=lines[i][1], )
+    test_input = ttk.Entry(test_frame, textvariable=lines[i][1], width=6)
     test_unit = ttk.Label(test_frame, text=lines[i][2])
-    test_name.grid(row=2+i, column=0)
-    test_input.grid(row=2+i, column=1)
+    test_name.grid(row=2+i, column=0, sticky='w')
+    test_input.grid(row=2+i, column=1, padx=5)
     test_unit.grid(row=2+i, column=2)
 
 def run_simulation(*args):
