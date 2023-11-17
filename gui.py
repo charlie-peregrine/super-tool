1c04ddaa1a2ecf6278097306d6f6fac5f8f90271
Charlie 3 Jordan <charlie@peregrineengineering.com>
2023-11-17 11:07:31 -0500

    moved everything into the main class, fixed the play button disappearing

--- gui.py
+++ gui.py
@@ -5,131 +5,127 @@ import tkinter as tk
 from tkinter import ttk
 from superbackend import *
 
-### Base window setup
 class SuperToolMain:
     def __init__(self, parent):
         self.parent = parent
+        ### setup root window options
+        self.parent.title("Super Tool")
+        # remove the tear off option from the top menu bar
+        self.parent.option_add('*tearOff', tk.FALSE)
+        # Allows the main frame to be resizeable with the window
+        self.parent.rowconfigure(0, weight=1)
+        self.parent.columnconfigure(0, weight=1)
 
 
+        ### Top bar menu setup
+        menubar = tk.Menu(self.parent)
+        self.parent.config(menu=menubar)
+        file_menu = tk.Menu(menubar)
+        about_menu = tk.Menu(menubar)
 
-root.title("Super Tool")
+        file_menu.add_command(label="New", accelerator="ctrl+n") #@TODO make the accelerators do something
+        file_menu.add_command(label="Save", accelerator="ctrl+s")
+        file_menu.add_command(label="Save As", command=save_as_project, accelerator="ctrl+shift+s")
+        file_menu.add_command(label="Open Project File", command=open_project)
+        file_menu.add_command(label="Open Workspace", command=open_workspace)
+        file_menu.add_separator()
+        file_menu.add_command(label='Exit', command=self.parent.destroy)
+        menubar.add_cascade(label="File", menu=file_menu)
 
-# remove the tear off option from the top menu bar
-root.option_add('*tearOff', tk.FALSE)
+        about_menu.add_command(label="About us")
+        about_menu.add_command(label="Version")
+        menubar.add_cascade(label="About", menu=about_menu)
 
-# Allows the main frame to be resizeable with the window
-root.rowconfigure(0, weight=1)
-root.columnconfigure(0, weight=1)
+        ### Frame creation
+        # main frame setup, row and column configure for initial size and resizeability
+        main_frame = ttk.Frame(self.parent, height=600, width=600)
+        main_frame.grid_rowconfigure(0, minsize=150, weight=1)
+        main_frame.grid_columnconfigure(0, minsize=50, weight=1)
+        main_frame.grid_rowconfigure(1, minsize=150, weight=1)
+        main_frame.grid_columnconfigure(1, minsize=50, weight=1)
+        main_frame.grid_columnconfigure(2, minsize=50, weight=1)
 
-### Top bar menu setup
-menubar = tk.Menu(root)
-root.config(menu=menubar)
-file_menu = tk.Menu(menubar)
-about_menu = tk.Menu(menubar)
+        # set up the sub-frames
+        proj_frame = ttk.Frame(main_frame, borderwidth=5, relief='groove',
+                            height=300, width=300)
+        test_frame = ttk.Frame(main_frame, borderwidth=5, relief='groove',
+                            height=100, width=100)
+        param_frame = ttk.Frame(main_frame, borderwidth=5, relief='groove',
+                            height=100, width=100)
+        plot_frame = ttk.Frame(main_frame, borderwidth=5, relief='groove',
+                            height=300, width=400)
+        statusbar_frame = ttk.Frame(main_frame, borderwidth=2, relief='groove') # nothing else so it's just a popup sort of deal
 
-file_menu.add_command(label="New", accelerator="ctrl+n") #@TODO make the accelerators do something
-file_menu.add_command(label="Save", accelerator="ctrl+s")
-file_menu.add_command(label="Save As", command=save_as_project, accelerator="ctrl+shift+s")
-file_menu.add_command(label="Open Project File", command=open_project)
-file_menu.add_command(label="Open Workspace", command=open_workspace)
-file_menu.add_separator()
-file_menu.add_command(label='Exit', command=root.destroy)
-menubar.add_cascade(label="File", menu=file_menu)
+        # place the frames in their grids, main_frame inside root and the rest
+        # inside main_frame
+        main_frame.grid(row=0,column=0, sticky="nesw")
+        proj_frame.grid(row=0,column=0, columnspan=1, rowspan=2, sticky="nesw")
+        test_frame.grid(row=0,column=1, columnspan=1, rowspan=1, sticky="nesw")
+        param_frame.grid(row=1,column=1, columnspan=1, rowspan=1, sticky="nesw")
+        plot_frame.grid(row=0,column=2, columnspan=1, rowspan=2, sticky="nesw")
+        statusbar_frame.grid(row=2, column=0, columnspan=3, sticky="nesw")
 
-about_menu.add_command(label="About us")
-about_menu.add_command(label="Version")
-menubar.add_cascade(label="About", menu=about_menu)
+        # example text inside the frames
+        # proj_text = ttk.Label(proj_frame, text="project")
+        # proj_text.grid(row=0,column=0)
+        # test_text = ttk.Label(test_frame, text="test")
+        # test_text.grid(row=0,column=0)
+        param_text = ttk.Label(param_frame, text="plot params")
+        param_text.grid(row=0,column=0)
+        statusbar_text = ttk.Label(statusbar_frame, text="Status Bar")
+        statusbar_text.grid(row=0, column=0)
 
-### Frame creation
-# main frame setup, row and column configure for initial size and resizeability
-main_frame = ttk.Frame(root, height=600, width=600)
-main_frame.grid_rowconfigure(0, minsize=150, weight=1)
-main_frame.grid_columnconfigure(0, minsize=50, weight=1)
-main_frame.grid_rowconfigure(1, minsize=150, weight=1)
-main_frame.grid_columnconfigure(1, minsize=50, weight=1)
-main_frame.grid_columnconfigure(2, minsize=50, weight=1)
+        ### Project Frame Details
+        proj_header = ttk.Label(proj_frame, text="Project Name") # @TODO font size up
+        proj_header.grid(row=0, column=0, columnspan=3)
+        project_title = tk.StringVar()
+        proj_header_entry = ttk.Entry(proj_frame, textvariable=project_title)
+        proj_header_entry.grid(row=0,column=3, columnspan=2)
 
-# set up the sub-frames
-proj_frame = ttk.Frame(main_frame, borderwidth=5, relief='groove',
-                       height=300, width=300)
-test_frame = ttk.Frame(main_frame, borderwidth=5, relief='groove',
-                       height=100, width=100)
-param_frame = ttk.Frame(main_frame, borderwidth=5, relief='groove',
-                       height=100, width=100)
-plot_frame = ttk.Frame(main_frame, borderwidth=5, relief='groove',
-                       height=300, width=400)
-statusbar_frame = ttk.Frame(main_frame, borderwidth=2, relief='groove') # nothing else so it's just a popup sort of deal
-
-# place the frames in their grids, main_frame inside root and the rest
-# inside main_frame
-main_frame.grid(row=0,column=0, sticky="nesw")
-proj_frame.grid(row=0,column=0, columnspan=1, rowspan=2, sticky="nesw")
-test_frame.grid(row=0,column=1, columnspan=1, rowspan=1, sticky="nesw")
-param_frame.grid(row=1,column=1, columnspan=1, rowspan=1, sticky="nesw")
-plot_frame.grid(row=0,column=2, columnspan=1, rowspan=2, sticky="nesw")
-statusbar_frame.grid(row=2, column=0, columnspan=3, sticky="nesw")
-
-# example text inside the frames
-# proj_text = ttk.Label(proj_frame, text="project")
-# proj_text.grid(row=0,column=0)
-# test_text = ttk.Label(test_frame, text="test")
-# test_text.grid(row=0,column=0)
-param_text = ttk.Label(param_frame, text="plot params")
-param_text.grid(row=0,column=0)
-statusbar_text = ttk.Label(statusbar_frame, text="Status Bar")
-statusbar_text.grid(row=0, column=0)
-
-### Project Frame Details
-proj_header = ttk.Label(proj_frame, text="Project Name") # @TODO font size up
-proj_header.grid(row=0, column=0, columnspan=3)
-project_title = tk.StringVar()
-proj_header_entry = ttk.Entry(proj_frame, textvariable=project_title)
-proj_header_entry.grid(row=0,column=3, columnspan=2)
-
-num_tests = 4
-project_test_entries = [tk.StringVar() for i in range(num_tests)]
-for i in range(num_tests):
-    ttk.Label(proj_frame, text="Test %d\n  Test type" % (i+1)).grid(
-        row=1+i, column=0, columnspan=2
-    )
-    ttk.Entry(proj_frame, textvariable=project_test_entries[i]).grid(
-        row=1+i, column=2, columnspan=3
-    )
+        num_tests = 4
+        project_test_entries = [tk.StringVar() for i in range(num_tests)]
+        for i in range(num_tests):
+            ttk.Label(proj_frame, text="Test %d\n  Test type" % (i+1)).grid(
+                row=1+i, column=0, columnspan=2
+            )
+            ttk.Entry(proj_frame, textvariable=project_test_entries[i]).grid(
+                row=1+i, column=2, columnspan=3
+            )
 
 
-### Test Parameters Box details
-test_header = ttk.Label(test_frame, text="Test Parameters:") # @TODO font size up 
-test_header.grid(row=0, column=0, columnspan=1, sticky="w")
+        ### Test Parameters Box details
+        test_header = ttk.Label(test_frame, text="Test Parameters:") # @TODO font size up 
+        test_header.grid(row=0, column=0, columnspan=1, sticky="w")
 
-def run_simulation(*args):
-    blah = ' '.join([i.get() for i in strings])
-    print(blah)
-    statusbar_text.config(text="Status Bar: " + blah)
+        def run_simulation(*args):
+            blah = ' '.join([i.get() for i in strings])
+            print(blah)
+            statusbar_text.config(text="Status Bar: " + blah)
 
-img = tk.PhotoImage(file="icons/supertoolplay.png")
-ttk.Button(test_frame, image=img, command=run_simulation).grid(row=0, column=2)
+        self.img = tk.PhotoImage(file="./icons/supertoolplay.png")
+        ttk.Button(test_frame, image=self.img, command=run_simulation).grid(row=0, column=2)
 
-# example input field to show that the entries work
-strings = [tk.StringVar() for i in range(4)]
+        # example input field to show that the entries work
+        strings = [tk.StringVar() for i in range(4)]
 
-# example of fields, might be better as a class instead of a tuple 
-lines = [("start time", strings[0], "sec"), ("end time", strings[1], "sec"),
-         ("step up size", strings[2], "pu"), ("step down size", strings[3], "pu")]
-for i in range(len(lines)):
-    test_name = ttk.Label(test_frame, text=lines[i][0])
-    test_input = ttk.Entry(test_frame, textvariable=lines[i][1], width=6)
-    test_unit = ttk.Label(test_frame, text=lines[i][2])
-    test_name.grid(row=2+i, column=0, sticky='w')
-    test_input.grid(row=2+i, column=1)
-    test_unit.grid(row=2+i, column=2)
+        # example of fields, might be better as a class instead of a tuple 
+        lines = [("start time", strings[0], "sec"), ("end time", strings[1], "sec"),
+                ("step up size", strings[2], "pu"), ("step down size", strings[3], "pu")]
+        for i in range(len(lines)):
+            test_name = ttk.Label(test_frame, text=lines[i][0])
+            test_input = ttk.Entry(test_frame, textvariable=lines[i][1], width=6)
+            test_unit = ttk.Label(test_frame, text=lines[i][2])
+            test_name.grid(row=2+i, column=0, sticky='w')
+            test_input.grid(row=2+i, column=1)
+            test_unit.grid(row=2+i, column=2)
 
 
 
 
-# these 2 lines set the minimum size of the window to its initial size
-# @TODO may need to mess with this later but it's fine for now
-root.update()
-root.minsize(root.winfo_width(), root.winfo_height())
+        # these 2 lines set the minimum size of the window to its initial size
+        # @TODO may need to mess with this later but it's fine for now
+        self.parent.update()
+        self.parent.minsize(self.parent.winfo_width(), self.parent.winfo_height())
 
 
 if __name__ == "__main__":
