035fc188ec711b9ccdb15b9986d911f57fc64167
Charlie 3 Jordan <charlie@peregrineengineering.com>
2023-11-30 10:00:25 -0500

    put everything besides variable setting into a run command

--- pslf_scripts/Voltage_Reference.py
+++ pslf_scripts/Voltage_Reference.py
@@ -54,70 +54,72 @@ Zbranch             = 0.09    # pu
 
 #--------------------------------------------------------------------------------------------------
 
+def run():
+    # gets the project directory of this file and initialize the PSLF instance
+    project_directory = os.path.dirname(os.path.realpath(__file__))
+    #os.chdir(project_directory)
+    SuperTool.launch_Pslf(project_directory)
+    SuperTool.pwd()
 
-# gets the project directory of this file and initialize the PSLF instance
-project_directory = os.path.dirname(os.path.realpath(__file__))
-#os.chdir(project_directory)
-SuperTool.launch_Pslf(project_directory)
-SuperTool.pwd()
-
-SuperTool.psds()
-SuperTool.getf(sav_filename)
-SuperTool.setf(Pinit,Qinit,MVAbase,Vinit,Vbase,Zbranch,set_loadflow,save_loadflow,sav_filename)
-SuperTool.soln(0)
-SuperTool.rdyd(dyd_filename, rep_filename, 1,0,1)   
-SuperTool.init(chf_filename,rep_filename,0,1,"","", 1)
-
-
-# Turn off PSS if not used in the simulation
-dp = DynamicsParameters()                               # gets all the dynamics parameers
-if(PSS_On==0):
-    SuperTool.turn_off_pss(dp)
-
-SuperTool.print_to_pslf("\n--- Establishing the Pre-Step State")
-dp.Delt = 1 / (SimPtsPerCycle * SysFreqInHz)            # sets the delta time step 
-dp.Conv_Mon = 0                                         # set to 1 to display convergence monitor
-dp.Nplot = round(SimPtsPerCycle)                        # save data every cycle
-dp.Nscreen = round(SysFreqInHz * SimPtsPerCycle)        #  display values on screen every 0.5 second
-dp.Tpause = StepTimeInSecs                              # pauses at the beginning of the voltage step
-ret = Pslf.run_dyn()
-
-SuperTool.print_to_pslf("\n--- Applying the Step")
-GeneratorInitialConditions[0].Vref = GeneratorInitialConditions[0].Vref + UpStepInPU
-dp.Nplot = 1
-dp.Nscreen = round(SysFreqInHz*SimPtsPerCycle)
-dp.Tpause = StepTimeInSecs + StepLenInSecs
-ret = Pslf.run_dyn()
-
-SuperTool.print_to_pslf("\n--- Removing the Step")
-GeneratorInitialConditions[0].Vref = GeneratorInitialConditions[0].Vref - DnStepInPU
-dp.Tpause = TotTimeInSecs
-ret = Pslf.run_dyn()
-ret = Pslf.end_dyn_run()
-ret = SuperTool.chf_to_csv(csv_filename)
-
-SuperTool.print_to_pslf("\n-----------------------------------------------------------------------------------------")
-SuperTool.print_to_pslf("Previous Voltage Reference Step Simulation Used:")
-SuperTool.print_to_pslf("Positive Step Size:  ", UpStepInPU," pu")
-SuperTool.print_to_pslf("Negative Step Size:  ", DnStepInPU," pu")
+    SuperTool.psds()
+    SuperTool.getf(sav_filename)
+    SuperTool.setf(Pinit,Qinit,MVAbase,Vinit,Vbase,Zbranch,set_loadflow,save_loadflow,sav_filename)
+    SuperTool.soln(0)
+    SuperTool.rdyd(dyd_filename, rep_filename, 1,0,1)   
+    SuperTool.init(chf_filename,rep_filename,0,1,"","", 1)
 
 
-i = Pslf.record_index(1,1,1,2,"1",1, -1)
-SuperTool.print_to_pslf("System Impedance:    ", Secdd[i].Zsecx)
+    # Turn off PSS if not used in the simulation
+    dp = DynamicsParameters()                               # gets all the dynamics parameers
+    if(PSS_On==0):
+        SuperTool.turn_off_pss(dp)
 
-if(PSS_On==1):
-    SuperTool.print_to_pslf("PSS Status:          On")
-else:
-    SuperTool.print_to_pslf("PSS Status:          Off")
+    SuperTool.print_to_pslf("\n--- Establishing the Pre-Step State")
+    dp.Delt = 1 / (SimPtsPerCycle * SysFreqInHz)            # sets the delta time step 
+    dp.Conv_Mon = 0                                         # set to 1 to display convergence monitor
+    dp.Nplot = round(SimPtsPerCycle)                        # save data every cycle
+    dp.Nscreen = round(SysFreqInHz * SimPtsPerCycle)        #  display values on screen every 0.5 second
+    dp.Tpause = StepTimeInSecs                              # pauses at the beginning of the voltage step
+    ret = Pslf.run_dyn()
+
+    SuperTool.print_to_pslf("\n--- Applying the Step")
+    GeneratorInitialConditions[0].Vref = GeneratorInitialConditions[0].Vref + UpStepInPU
+    dp.Nplot = 1
+    dp.Nscreen = round(SysFreqInHz*SimPtsPerCycle)
+    dp.Tpause = StepTimeInSecs + StepLenInSecs
+    ret = Pslf.run_dyn()
+
+    SuperTool.print_to_pslf("\n--- Removing the Step")
+    GeneratorInitialConditions[0].Vref = GeneratorInitialConditions[0].Vref - DnStepInPU
+    dp.Tpause = TotTimeInSecs
+    ret = Pslf.run_dyn()
+    ret = Pslf.end_dyn_run()
+    ret = SuperTool.chf_to_csv(csv_filename)
+
+    SuperTool.print_to_pslf("\n-----------------------------------------------------------------------------------------")
+    SuperTool.print_to_pslf("Previous Voltage Reference Step Simulation Used:")
+    SuperTool.print_to_pslf("Positive Step Size:  ", UpStepInPU," pu")
+    SuperTool.print_to_pslf("Negative Step Size:  ", DnStepInPU," pu")
 
 
-SuperTool.print_to_pslf("Set loadflow?        ", bool(set_loadflow))
-SuperTool.print_to_pslf("Save loadflow?       ", bool(save_loadflow))
-SuperTool.print_to_pslf("Sav Filename:        ", sav_filename)
-SuperTool.print_to_pslf("Dyd Filename:        ", dyd_filename)
-SuperTool.print_to_pslf("Chf Filename:        ", chf_filename)
-SuperTool.print_to_pslf("Csv Filename:        ", csv_filename)
-SuperTool.print_to_pslf("Rep Filename:        ", rep_filename)
-SuperTool.print_to_pslf("-----------------------------------------------------------------------------------------")
+    i = Pslf.record_index(1,1,1,2,"1",1, -1)
+    SuperTool.print_to_pslf("System Impedance:    ", Secdd[i].Zsecx)
 
-print("Finished running Voltage_Reference.py script!\n")
+    if(PSS_On==1):
+        SuperTool.print_to_pslf("PSS Status:          On")
+    else:
+        SuperTool.print_to_pslf("PSS Status:          Off")
+
+
+    SuperTool.print_to_pslf("Set loadflow?        ", bool(set_loadflow))
+    SuperTool.print_to_pslf("Save loadflow?       ", bool(save_loadflow))
+    SuperTool.print_to_pslf("Sav Filename:        ", sav_filename)
+    SuperTool.print_to_pslf("Dyd Filename:        ", dyd_filename)
+    SuperTool.print_to_pslf("Chf Filename:        ", chf_filename)
+    SuperTool.print_to_pslf("Csv Filename:        ", csv_filename)
+    SuperTool.print_to_pslf("Rep Filename:        ", rep_filename)
+    SuperTool.print_to_pslf("-----------------------------------------------------------------------------------------")
+
+    print("Finished running Voltage_Reference.py script!\n")
+
+run()
