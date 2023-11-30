cbbd2c1bfe7c15b8030ea2322fe8c4885ad79c26
Charlie 3 Jordan <charlie@peregrineengineering.com>
2023-11-30 09:58:12 -0500

    restructured the file setup to have the project frame section be in its own file, also added context menus for tests that can adequately rename and delete tests

--- SuperToolMenus.py
+++ SuperToolMenus.py
@@ -8,36 +8,3 @@ from superbackend import *
 from SuperToolFrames import *
 import SuperToolProject as stp
 
-class UnitLabel(ttk.Label):
-    def __init__(self, parent, **kwargs):
-        self.parent = parent
-        super().__init__(self.parent, **kwargs)
-        
-        self.menu = ProjectUnitMenu(self)
-        self.bind('<3>', lambda e: self.menu.post(e.x_root, e.y_root))
-
-class TestLabel(ttk.Label):
-    def __init__(self, parent, **kwargs):
-        self.parent = parent
-        super().__init__(self.parent, **kwargs)
-        
-        self.menu = ProjectTestMenu(self)
-        self.bind('<3>', lambda e: self.menu.post(e.x_root, e.y_root))
-
-
-class ProjectUnitMenu(tk.Menu):
-    def __init__(self, parent, **kwargs):
-        self.parent = parent
-        super().__init__(self.parent)
-        
-        self.add_command(label="Rename Unit")
-        self.add_command(label="Delete Unit")
-        self.add_command(label="Add Test")
-
-class ProjectTestMenu(tk.Menu):
-    def __init__(self, parent, **kwargs):
-        self.parent = parent
-        super().__init__(self.parent)
-        
-        self.add_command(label="Rename Test")
-        self.add_command(label="Delete Test")
