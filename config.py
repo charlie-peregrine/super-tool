81176161bd6972a0557d41851725f8536a29841c
Charlie Jordan <charlie@peregrineengineering.com>
2024-01-17 09:58:50 -0500

    add more robust configuration checks including...
    adding veusz to path, getting default veusz location if possible,
    installing pywin32 if necessary, and printing out progress

--- config.py
+++ config.py
@@ -2,16 +2,67 @@
 
 
 import json
+from tkinter.messagebox import showwarning, showinfo
 
 if __name__ == '__main__':
-    from tkinter.filedialog import askdirectory
-    dir = askdirectory(title="Select the directory containing veusz.exe")
-    json.dump({"VEUSZ_PATH" : dir}, open('config.json', 'w'), indent=4)
+    from tkinter.filedialog import askopenfilename
+    import os
+    import subprocess
+    import time
+    
+    try:
+        import win32
+    except ModuleNotFoundError:
+        print("===== Installing pywin32 =====")
+        subprocess.call("python -m pip install pywin32")
+        print("===== pywin32 Installed!")
+    
+    print("===== Finding veusz =====")
+    where_output = subprocess.check_output("where veusz").decode('utf-8').strip()
+    dir = ''
+    if where_output:
+        print("===== Veusz Found!")
+        if '\n' in where_output:
+            where_output = where_output.split('\n')[0]
+        dir = askopenfilename(
+                title="Find and select veusz.exe on your computer",
+                initialdir=os.path.dirname(where_output)
+                )
+        dir = os.path.dirname(dir)
+    else:
+        print("===== Veusz Not Found")
+        dir = askopenfilename(
+                title="Find and select veusz.exe on your computer"
+                )
+        dir = os.path.dirname(dir)
 
+    print("===== Finding Veusz on PATH =====")
+    user_path = subprocess.check_output("echo %PATH%", shell=True).decode('utf-8')
+    win_dir = dir.replace('/','\\')
+    if win_dir not in user_path:
+        print("===== Veusz not found on PATH. Prompt user.")
+        showinfo(title="Add to path",
+                    message=f"On the next window please add\n{dir}\nto your user or system path variable.")
+        print(win_dir)
+        subprocess.call(r"C:\WINDOWS\system32\rundll32.exe sysdm.cpl,EditEnvironmentVariables", shell=True)
+    else:
+        print("===== Veusz found on PATH!")
+        
+    
+    json.dump({"VEUSZ_PATH" : dir}, open('config.json', 'w'), indent=4)
+    print("Configuration Saved!")
+    showinfo(title="Configuration complete",
+             message="Configuration complete!\nClick Ok to finish.")
+    
+
+print("===== loading config.ini =====")
 try:
     config_data = json.load(open('config.json', 'r'))
 except FileNotFoundError:
-    print("No Configuration file created yet. Please run config.py first.")
+    showwarning(title="Un-configured Installation",
+        message="No Configuration file created yet. Please run config.py first.")
     exit()
 
 VEUSZ_PATH = config_data['VEUSZ_PATH']
+
+print("===== config.ini loaded =====")
